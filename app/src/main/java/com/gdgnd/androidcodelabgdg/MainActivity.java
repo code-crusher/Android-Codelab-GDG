372b2a0cdd624698651c2774651b9653dda7de83
appyware <vatsal.bajpai05@gmail.com>
2015-10-17 08:40:34 +0530

    Added Contacts

--- app/src/main/java/com/gdgnd/androidcodelabgdg/MainActivity.java
+++ app/src/main/java/com/gdgnd/androidcodelabgdg/MainActivity.java
@@ -1,34 +1,84 @@
 package com.gdgnd.androidcodelabgdg;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout firstNameLayout, secondNameLayout;
+    private EditText firstName, surName;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        firstNameLayout = (TextInputLayout) findViewById(R.id.firstNameLayout);
+        secondNameLayout = (TextInputLayout) findViewById(R.id.secondNameLayout);
+        firstName = (EditText) findViewById(R.id.firstName);
+        surName = (EditText) findViewById(R.id.surName);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                storeValues(view);
             }
         });
+
+        setStoredValues();
     }
 
+    private void storeValues(View view) {
+        if (firstName.getText().toString().trim().length() == 0) {
+            firstNameLayout.setError("First Name is required*");
+            return;
+        } else if (surName.getText().toString().trim().length() == 0) {
+            secondNameLayout.setError("Surname is required*");
+            return;
+        } else {
+            SharedPreferences prefs = this.getSharedPreferences("stored", MODE_PRIVATE);
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putString("firstname", firstName.getText().toString());
+            edit.putString("surname", surName.getText().toString());
+            edit.apply();
+            Snackbar.make(view, "Details Saved!", Snackbar.LENGTH_LONG)
+                    .setAction("Clear", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            clearValues();
+                            setStoredValues();
+                        }
+                    }).show();
+        }
+    }
+
+    private void setStoredValues() {
+        SharedPreferences prefs = this.getSharedPreferences("stored", MODE_PRIVATE);
+        firstName.setText(prefs.getString("firstname", ""));
+        surName.setText(prefs.getString("surname", ""));
+    }
+
+    private void clearValues() {
+        SharedPreferences prefs = this.getSharedPreferences("stored", MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("firstname", "");
+        edit.putString("surname", "");
+        edit.putString("address", "");
+        edit.apply();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
